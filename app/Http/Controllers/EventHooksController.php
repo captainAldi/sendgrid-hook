8d6a5a70482b7da7ff6e94711fb110dc09205898
captainAldi <renaldiyulvianda@yahoo.com>
2023-09-07 11:35:59 +0700

    fix import db facades

--- app/Http/Controllers/EventHooksController.php
+++ app/Http/Controllers/EventHooksController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\DB;
 
 use SendGrid\EventWebhook\EventWebhook;
 use SendGrid\EventWebhook\EventWebhookHeader;
 
-use Illuminate\Support\Facades\Mail;
 use App\Mail\TesKirimEmail;
 use App\Models\DeliveryEvent;
 
